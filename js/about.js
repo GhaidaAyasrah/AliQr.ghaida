c883eae780bdb7a713e741dc6cb5f0ef3959fdbe
GhaidaAyasrah <ghaida13ayasrah@gmail.com>
2025-05-06 23:13:10 +0300

    still make response

---
+++ js/about.js
@@ -0,0 +1,9 @@
+var callbtn = document.getElementById('callbtn');
+console.log(callbtn);   
+
+    callbtn.addEventListener('click', function callus() {
+        alert('calling...');
+        callbtn.innerHTML = "calling..."
+        });
+
+    
--- js/mobile.js
+++ js/mobile.js
@@ -1,6 +1,15 @@
 var contactbtn = document.getElementById('contactbtn');
 console.log(contactbtn);
-function contactbtn() { 
+function contactus() { 
     contactbtn.innerHTML = "contacting...";
     contactbtn.style.backgroundColor = "#green";
 }
+contactbtn.addEventListener('click', contactus);
+console.log("contactbtn clicked");
+var contactbtn = document.getElementById('contactbtn');
+console.log(contactbtn);
+function contactus() { 
+    contactbtn.innerHTML = "contacting...";
+    contactbtn.style.backgroundColor = "#green";
+}
+ 
