ffa7f875b90ac8ae571fdb1d8891758d299970e0
GhaidaAyasrah <ghaida13ayasrah@gmail.com>
2025-05-07 01:28:59 +0300

    final

--- js/about.js
+++ js/about.js
@@ -6,4 +6,4 @@ console.log(callbtn);
         callbtn.innerHTML = "calling..."
         });
 
-    
+       
--- js/mobile.js
+++ js/mobile.js
@@ -2,14 +2,5 @@ var contactbtn = document.getElementById('contactbtn');
 console.log(contactbtn);
 function contactus() { 
     contactbtn.innerHTML = "contacting...";
-    contactbtn.style.backgroundColor = "#green";
+    contactbtn.style.backgroundColor = "green";
 }
-contactbtn.addEventListener('click', contactus);
-console.log("contactbtn clicked");
-var contactbtn = document.getElementById('contactbtn');
-console.log(contactbtn);
-function contactus() { 
-    contactbtn.innerHTML = "contacting...";
-    contactbtn.style.backgroundColor = "#green";
-}
- 
